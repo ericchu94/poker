4c5f353bdd2531e98c32cbf6fa8cb87ed77bba67
Eric Chu <eric@ericchu.net>
2016-04-28 05:12:11 -0400

    Initial commit

---
+++ assets/index.js
@@ -0,0 +1,56 @@
+'use strict';
+$(function () {
+  $('#user').on('keyup', function (event) {
+    if (event.keyCode == 13) {
+      $('#add').click();
+    }
+  });
+
+  $('#add').on('click', function () {
+    $.ajax('/users', {
+      method: 'PUT',
+      data: {
+        user: $('#user').val(),
+      },
+    }).then(function () {
+      $('#user').val('');
+      $('#container').load(location.href + ' #content');
+    }, function (err) {
+      console.log(err);
+    });
+  });
+
+  $('#container').on('click', '.bet', function () {
+    var amount = $(this).val();
+    var user = $(this).parents('.user').find('h2').text();
+    $.ajax('/bet', {
+      method: 'POST',
+      data: {
+        user: user,
+        amount: amount,
+      },
+    }).then(function () {
+      $('#container').load(location.href + ' #content');
+    }, function (err) {
+      console.log(err);
+    });
+  });
+
+  $('#container').on('click', '.win', function () {
+    var user = $(this).parents('.user').find('h2').text();
+    $.ajax('/win', {
+      method: 'POST',
+      data: {
+        user: user,
+      },
+    }).then(function () {
+      $('#container').load(location.href + ' #content');
+    }, function (err) {
+      console.log(err);
+    });
+  });
+
+  //setInterval(function () {
+  //  $('#container').load(location.href + ' #content');
+  //}, 500);
+});
---
+++ index.js
@@ -0,0 +1,89 @@
+'use strict';
+const Koa = require('koa');
+const co = require('co');
+const router = require('koa-router')();
+const views = require('koa-views');
+const serve = require('koa-static');
+const bodyParser = require('koa-bodyparser');
+const app = new Koa();
+
+app.use(co.wrap(function *(ctx, next) {
+  const start = new Date();
+  yield next();
+  const end = new Date();
+  console.log(`${ctx.method} ${ctx.url} - ${ctx.status} ${end - start} ms`);
+}));
+
+app.use(co.wrap(function *(ctx, next) {
+  yield next();
+  console.log({
+    users: app.context.users,
+  });
+}));
+
+app.use(views(__dirname + '/views', {
+  map: {
+    ejs: 'ejs',
+  },
+  extension: 'ejs',
+}));
+
+router.get('/assets/*', serve('.'));
+
+router.get('/', co.wrap(function *(ctx, next) {
+  yield ctx.render('index', {
+    users: app.context.users,
+    pot: app.context.pot,
+  });
+}));
+
+router.put('/users', bodyParser(), co.wrap(function *(ctx, next) {
+  const body = ctx.request.body;
+  const users = app.context.users;
+  const user = body.user;
+
+  if (!(user in users)) {
+    users[user] = {
+      name: user,
+      score: 100,
+      bet: 0,
+    };
+    ctx.status = 200;
+    yield next();
+  }
+}));
+
+router.post('/bet', bodyParser(), co.wrap(function *(ctx, next) {
+  const body = ctx.request.body;
+  const user = app.context.users[body.user]; 
+  const amount = parseInt(body.amount);
+
+  if (user.score - amount >= 0) {
+    user.score -= amount;
+    user.bet += amount;
+    app.context.pot += amount;
+    ctx.status = 200;
+    yield next();
+  }
+}));
+
+router.post('/win', bodyParser(), co.wrap(function *(ctx, next) {
+  const body = ctx.request.body;
+  const users = app.context.users;
+
+  users[body.user].score += app.context.pot;
+  for (const user in users)
+    users[user].bet = 0;
+
+  app.context.pot = 0;
+  ctx.status = 200;
+  yield next();
+}));
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+app.context.users = {};
+app.context.pot = 0;
+
+app.listen(3000);
